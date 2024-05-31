3fba72c932fc01bfa773e739c9618baa0900cff4
wangwei <wwdqq7@qq.com>
2024-05-31 21:11:00 +0800

    feat(App.vue): 添加启动页面延迟关闭功能

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
+<!--
+ * @Author: wangwei wwdqq7@qq.com
+ * @Date: 2024-05-31 18:01:20
+ * @LastEditors: wangwei wwdqq7@qq.com
+ * @LastEditTime: 2024-05-31 21:10:21
+ * @FilePath: /college-life/src/App.vue
+ * @Description: 编写一个uni-app项目，在启动时延迟6秒后关闭启动页面。
+-->
 <script setup lang="ts">
 import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
 onLaunch(() => {
   console.log("App Launch");
+
+  // 设置一个定时器，延迟6秒后执行
+  const timeout = setTimeout(() => {
+    // 清除定时器
+    clearTimeout(timeout);
+
+    // 关闭启动页面
+    plus.navigator.closeSplashscreen();
+  }, 6000);
 });
 onShow(() => {
   console.log("App Show");
