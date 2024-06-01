8910914faa0e10eddcbd3a989f43cb8560a5e144
wangwei <wwdqq7@qq.com>
2024-06-02 02:33:44 +0800

    feat: 添加.npmrc配置和VSCode扩展；更新typescript声明文件

---
+++ src/locale/locale.d.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: wangwei wwdqq7@qq.com
+ * @Date: 2024-06-02 02:31:09
+ * @LastEditors: wangwei wwdqq7@qq.com
+ * @LastEditTime: 2024-06-02 02:31:21
+ * @FilePath: /college-life/src/locale/locale.d.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+declare module "@/locale/index";
---
+++ src/uview-plus.d.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: wangwei wwdqq7@qq.com
+ * @Date: 2024-06-02 02:29:41
+ * @LastEditors: wangwei wwdqq7@qq.com
+ * @LastEditTime: 2024-06-02 02:30:03
+ * @FilePath: /college-life/src/uview-plus.d.ts
+ * @Description:
+ */
+declare module "@/uni_modules/uview-plus";
