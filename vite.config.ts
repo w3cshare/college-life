3bdc4b4afb344ddcc28ad932b4a5ca902d40ee94
wangwei <wwdqq7@qq.com>
2024-06-02 00:43:12 +0800

    feat: 添加vite-plugin-html-create-at插件记录发布时间信息

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
+import htmlCreateAt from "./lib/vite-plugin-html-create-at";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [uni(), htmlCreateAt()],
 });
